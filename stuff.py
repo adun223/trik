acbb2d31c00cbf34fa8e77766dce7d4bc5cbf2ea
Dhihaey <daria.ananich@gmail.com>
2023-04-11 18:22:21 +0300

    Adding get_weather_funktion

--- stuff.py
+++ stuff.py
@@ -26,23 +26,31 @@ def send_image():
     return img_url
 
 
-def get_city(lat, lon):
-    locator = geopy.geocoders.Nominatim(user_agent='geoapiExercises')
-    location = locator.reverse(str(lat) + ',' + str(lon))
-    address = location.raw['address']
-    return address
-
-
 def get_forecast(lat, lon):
     url = 'https://api.openweather.org/data/2.5/forecast'
     params = {
-        'lat' : lat,
-        'lon' : lon,
-        'appid' : os.environ.get('weather_app'),
-        'units' : 'metric',
-        'lang' : 'ru',
+        'lat': lat,
+        'lon': lon,
+        'appid': os.environ.get('weather_app'),
+        'units': 'metric',
+        'lang': 'ru',
     }
     resp = r.get(url, params=params).json()
+    text = '<strong>{}</strong> <i>{}</i>: \n{}^C, {}\n\n'
+    res = ''
+
+    for data in resp['list']:
+        date = datetime.fromtimestamp(data['dt'])
+        date_res = date.strftime('%d.%m.%Y')
+        temp = data['main']['temp']
+        weather = temp['weather'][0]['desctiption']['weather']
+
+        if date.hour ==15:
+            daytime = 'днём'
+            res += text.format(date_res, daytime, temp, weather)
+        elif date.hour == 21:
+            daytime = 'вечером'
+            res += text.format(date_res, daytime, temp, weather)
     return resp
 
 
